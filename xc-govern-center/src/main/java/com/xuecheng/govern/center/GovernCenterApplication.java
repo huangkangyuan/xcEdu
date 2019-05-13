6c3a855664dfaa33ab1a0b5ed4a5188f3f861170
root <614772276@qq.com>
2019-05-13 11:30:56 +0800

    eureka注册中心

---
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.govern.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer//标识这是一个EurekaServer服务
+@SpringBootApplication
+public class GovernCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GovernCenterApplication.class, args);
+    }
+}
