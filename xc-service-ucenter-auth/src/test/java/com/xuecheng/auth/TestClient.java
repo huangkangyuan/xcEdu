37966633a1ea8d03d950539366b96ab3eac0b7e3
root <614772276@qq.com>
2019-06-02 09:40:38 +0800

    远程请求spring security获取令牌

---
+++ xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/TestClient.java
@@ -0,0 +1,84 @@
+package com.xuecheng.auth;
+
+import com.xuecheng.framework.client.XcServiceList;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestClient {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    //远程请求spring security获取令牌
+    @Test
+    public void testClient(){
+        //从eureka中获取认证服务的地址（因为spring security在认证服务中）
+        //从eureka中获取认证服务的一个实例的地址
+        ServiceInstance serviceInstance = loadBalancerClient.choose(XcServiceList.XC_SERVICE_UCENTER_AUTH);
+        //此地址就是http://ip:port
+        URI uri = serviceInstance.getUri();
+        //令牌申请的地址 http://localhost:40400/auth/oauth/token
+        String authUrl = uri+ "/auth/oauth/token";
+        //定义header
+        LinkedMultiValueMap<String, String> header = new LinkedMultiValueMap<>();
+        String httpBasic = getHttpBasic("XcWebApp", "XcWebApp");
+        header.add("Authorization",httpBasic);
+
+        //定义body
+        LinkedMultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type","password");
+        body.add("username","itcast");
+        body.add("password","123");
+
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, header);
+        //String url, HttpMethod method, @Nullable HttpEntity<?> requestEntity, Class<T> responseType, Object... uriVariables
+
+        //设置restTemplate远程调用时候，对400和401不让报错，正确返回数据
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler(){
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                if(response.getRawStatusCode()!=400 && response.getRawStatusCode()!=401){
+                    super.handleError(response);
+                }
+            }
+        });
+
+        ResponseEntity<Map> exchange = restTemplate.exchange(authUrl, HttpMethod.POST, httpEntity, Map.class);
+
+        //申请令牌信息
+        Map bodyMap = exchange.getBody();
+        System.out.println(bodyMap);
+    }
+
+    //获取httpbasic的串
+    private String getHttpBasic(String clientId,String clientSecret){
+        String string = clientId+":"+clientSecret;
+        //将串进行base64编码
+        byte[] encode = Base64Utils.encode(string.getBytes());
+        return "Basic "+new String(encode);
+    }
+
+}
