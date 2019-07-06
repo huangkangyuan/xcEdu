b32a096b098a4d6048e17acebe70efbaa31bbc0e
root <614772276@qq.com>
2019-07-06 22:32:05 +0800

    h2 test

---
+++ test-mt/src/main/java/com/xuecheng/mt/TestMtApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMtApplication.class, args);
+    }
+
+}
